bfe7bd83cf93b0f9f8e7c9960ebf5c052bb0716f
Wurst1one <henrique.marchini.@gmail.com>
2025-04-06 20:18:26 -0300

    Arrumando layout mobile

--- src/components/ExperienciaProfissional.jsx
+++ src/components/ExperienciaProfissional.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import '../styles/ExperienciaProfissional.css';
-import psicologaClinicaImg from '../assets/woman.svg'; // Imagem "abraço"
-import hospitalSaoLucasImg from '../assets/Telephone.svg'; // Imagem "telefone"
+import psicologaClinicaImg from '../assets/woman.svg';
+import hospitalSaoLucasImg from '../assets/Telephone.svg';
 
 const ExperienciaProfissional = () => {
   return (
     <section className="experiencia-profissional" id="experiencia-profissional">
       <h2>Experiência Profissional</h2>
-      <div className="experiencia-item psicologa-clinica">
-        <div className="experiencia-texto">
-          <h3>Psicóloga Clínica (2010 - Presente)</h3>
-          <ul>
-            <li>Atendimento individual e em grupo, com foco em saúde mental e bem-estar emocional.</li>
-            <li>Desenvolvimento de planos terapêuticos personalizados para cada paciente.</li>
-            <li>Supervisão de estagiários e colaboração com outros profissionais da saúde.</li>
-          </ul>
-        </div>
-        <img src={psicologaClinicaImg} alt="Psicóloga Clínica" />
-      </div>
+      
       <div className="experiencia-item hospital-sao-lucas">
         <div className="experiencia-texto">
           <h3>Hospital São Lucas (2008 - 2010)</h3>
@@ -27,7 +17,19 @@ const ExperienciaProfissional = () => {
             <li>Participação em equipes multidisciplinares para planejamento de alta hospitalar.</li>
           </ul>
         </div>
-        <img src={hospitalSaoLucasImg} alt="Hospital São Lucas" />
+        <img src={hospitalSaoLucasImg} alt="Hospital São Lucas" className="icone-experiencia"/>
+      </div>
+
+      <div className="experiencia-item psicologa-clinica">
+        <div className="experiencia-texto">
+          <h3>Psicóloga Clínica (2010 - Presente)</h3>
+          <ul>
+            <li>Atendimento individual e em grupo, com foco em saúde mental e bem-estar emocional.</li>
+            <li>Desenvolvimento de planos terapêuticos personalizados para cada paciente.</li>
+            <li>Supervisão de estagiários e colaboração com outros profissionais da saúde.</li>
+          </ul>
+        </div>
+        <img src={psicologaClinicaImg} alt="Psicóloga Clínica" className="icone-experiencia"/>
       </div>
     </section>
   );
--- src/components/FormacaoAcademica.jsx
+++ src/components/FormacaoAcademica.jsx
@@ -5,15 +5,15 @@ import iconeCerebro from '../assets/Brain.svg';
 const FormacaoAcademica = () => {
   return (
     <section className="formacao-academica">
-      <div className="formacao-titulo">
-        <h2 data-aos="fade-up">Formação Acadêmica</h2>
+      <div className="formacao-titulo aos-init aos-animate" data-aos="fade-up">
+        <h2>Formação Acadêmica</h2>
         <div className="formacao-lista" data-aos="fade-up" data-aos-delay="100">
           <p>Graduação em Psicologia - Universidade XYZ, 2008</p>
           <p>Pós-Graduação em Terapia Cognitivo-Comportamental - Instituto ABC, 2011</p>
           <p>Mestrado em Psicologia Clínica - Universidade DEF, 2015</p>
         </div>
       </div>
-      <div className="formacao-imagem" data-aos="fade-right">
+      <div className="formacao-imagem aos-init aos-animate" data-aos="fade-right">
         <img src={iconeCerebro} alt="Ícone de Cérebro" />
       </div>
     </section>
